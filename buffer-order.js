cbc276e9bc141cd5340b3f736fdf9d10065336b1
Guobao Jiang <loveaborn@foxmail.com>
2014-03-18 16:55:48 +0800

    add buffer-order next buffer for new link

---
+++ buffer-order.js
@@ -0,0 +1,4 @@
+new_buffer_position = 0;
+new_buffer_with_opener_position = buffer_position_after;
+bury_buffer_position = buffer_position_end_by_type;
+special_buffer.prototype.default_position = buffer_position_end_by_type;
